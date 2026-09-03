64fc5b46675459a95467c2104a2800ccd9053691
Yamanqui García Rosales <yamanqui@gmail.com>
2026-09-02 20:43:02 -0700

    Moved from ts-jest into @swc/jest in praparation for typescript 7

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,12 +1,8 @@
-const { createDefaultPreset } = require("ts-jest");
-
-const tsJestTransformCfg = createDefaultPreset().transform;
-
 /** @type {import("jest").Config} **/
 module.exports = {
     testEnvironment: "node",
     transform: {
-        ...tsJestTransformCfg,
+        "^.+\\.(t|j)sx?$": "@swc/jest",
     },
     testPathIgnorePatterns: [
         "/node_modules/",
